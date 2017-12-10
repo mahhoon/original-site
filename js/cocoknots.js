4eeb4835767bd1b00409181f65e79c7ea29d349e
mahon <mahon@ya2.so-net.ne.jp>
2017-12-10 03:21:55 +0000

    8th commit

--- js/cocoknots.js
+++ js/cocoknots.js
@@ -8,3 +8,17 @@ $(function(){
             $("ul",this).slideUp("fast");
     })
 }) 
+
+/*グロナビ固定*/
+
+$(function(){
+    var nav = $('#header'),
+	offset = nav.offset();
+	$(window).scroll(function () {
+	  if($(window).scrollTop() > offset.top) {
+	    nav.addClass('fixed');
+	  } else {
+	    nav.removeClass('fixed');
+	  }
+	});
+});
