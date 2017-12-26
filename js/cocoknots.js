f31d577fc83ced82f2f7860021a92f0a9a8ab54d
mahon <mahon@ya2.so-net.ne.jp>
2017-12-26 05:58:19 +0000

    12th commit

--- js/cocoknots.js
+++ js/cocoknots.js
@@ -22,3 +22,44 @@ $(function(){
 	  }
 	});
 });
+
+/*サイドナビ固定*/
+$(function() {
+     $('#sidemenu').fitSidebar({
+         wrapper : '#contents',
+         responsiveWidth : 767
+     });
+ });
+
+/*
+ $(function(){
+    var fix = $('#sidemenu'), //固定したいコンテンツ
+    side = $('#sidebar'), //サイドバーのID
+    main = $('#content'), //固定する要素を収める範囲
+     sideTop = side.offset().top;
+     fixTop = fix.offset().top,
+     mainTop = main.offset().top,
+     w = $(window);
+      
+     var adjust = function(){
+     fixTop = fix.css('position') === 'static' ? sideTop + fix.position().top : fixTop;
+     var fixHeight = fix.outerHeight(true),
+     mainHeight = main.outerHeight(),
+     winTop = w.scrollTop();
+     if(winTop + fixHeight > mainTop + mainHeight){
+     fix.css({
+     position: 'absolute',
+     top: mainHeight - fixHeight
+     });
+     }else if(winTop >= fixTop){
+     fix.css({
+     position: 'fixed',
+     top: 10
+     });
+     }else{
+     fix.css('position', 'static');
+     }
+     }
+     w.on('scroll', adjust);
+ });
+ */
